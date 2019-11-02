a839901f0f5e7a1dc8b6976568cf01fd5b6d0a48
raju-ahammad <web.rajuahammad.com>
2019-11-02 20:48:26 +0600

    heroku config

--- order/views.py
+++ order/views.py
@@ -41,14 +41,13 @@ def order_create(request):
 
 
 
-@staff_member_required
+
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request,
                   'admin/orders/order/detail.html',
                   {'order': order})
 
-@staff_member_required
 def admin_order_pdf(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     html = render_to_string('order/pdf.html',
